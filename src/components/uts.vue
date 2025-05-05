41fcea562565483e0e8c7e8f9cd4e89c2e9ed1b0
sandyyy711140 <Sandyyysatriaaa@gmail.com>
2025-05-05 14:18:09 +0700

    membuat user dapat melihat kegiatan

--- src/components/uts.vue
+++ src/components/uts.vue
@@ -1,1 +1,49 @@
-git
+<script>
+export default {
+  name: 'GamePurchaseList',
+  data() {
+    return {
+      games: [],
+    };
+  },
+  mounted() {
+    this.loadGames();
+  },
+  methods: {
+    formatPrice(value) {
+      if (!value) return "0";
+      return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    },
+    loadGames() {
+      try {
+        const savedGames = localStorage.getItem('purchasedGames');
+        if (savedGames) {
+          this.games = JSON.parse(savedGames);
+        }
+      } catch (e) {
+        console.error("Failed to load data:", e);
+      }
+    },
+  },
+};
+</script>
+
+<template>
+    <div>
+      <h1>Game Purchase List</h1>
+      <div>
+        <ul>
+          <li
+            v-for="(game, index) in games"
+            :key="game.id"
+          >
+            <span>{{ game.name }}</span> - <span>Rp {{ formatPrice(game.price) }}</span>
+          </li>
+        </ul>
+        <p v-if="games.length === 0">No games added yet</p>
+      </div>
+    </div>
+  </template>
+  
+
+  
